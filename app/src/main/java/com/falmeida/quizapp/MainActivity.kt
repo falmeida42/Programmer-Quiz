3badadedb65db148b9890b62a624af42a3cc010c
falmeida <flaviomslima@gmail.com>
2021-11-12 19:18:49 -0300

    Login Quiz App

--- app/src/main/java/com/falmeida/quizapp/MainActivity.kt
+++ app/src/main/java/com/falmeida/quizapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.falmeida.quizapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_start).setOnClickListener {
+            if (findViewById<EditText>(R.id.et_name).text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }else {
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
