d99113d8fe7cd7194b8ab52666630b04e6af619b
falmeida <flaviomslima@gmail.com>
2021-11-14 10:30:31 -0300

    Login Quiz App Questions

--- app/src/main/java/com/falmeida/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/falmeida/quizapp/QuizQuestionActivity.kt
@@ -2,6 +2,9 @@ package com.falmeida.quizapp
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class QuizQuestionActivity : AppCompatActivity() {
@@ -11,5 +14,18 @@ class QuizQuestionActivity : AppCompatActivity() {
 
         val questionsList = Constants.getQuestions()
         Log.i("Questions Size", "${questionsList.size}")
+
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition - 1]
+
+        findViewById<ProgressBar>(R.id.progressBar).progress = currentPosition
+        findViewById<TextView>(R.id.tv_progress).text = "$currentPosition" + "/" + findViewById<ProgressBar>(R.id.progressBar).max
+
+        findViewById<TextView>(R.id.tv_question).text = question!!.question
+        findViewById<ImageView>(R.id.iv_image).setImageResource(question.image)
+        findViewById<TextView>(R.id.tv_option_one).text = question.optionOne
+        findViewById<TextView>(R.id.tv_option_two).text = question.optionTwo
+        findViewById<TextView>(R.id.tv_option_three).text = question.optionThree
+        findViewById<TextView>(R.id.tv_option_four).text = question.optionFour
     }
 }
